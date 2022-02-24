ab8a67dd524404581edd273c0823624d77aa2757
luv4code-lc <madhav.p.akv@gmail.com>
2022-02-24 10:26:33 +0530

    creating initial project. closes #1

---
+++ src/test/java/com/luv4code/beerservice/BeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv4code.beerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
