4ce01b795e70d1aa2027a513e6f976272b820f06
luv4code-lc <madhav.p.akv@gmail.com>
2022-02-25 09:48:58 +0530

    Fixing tests. closes #11

--- src/test/java/com/luv4code/beerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/luv4code/beerservice/web/controller/BeerControllerTest.java
@@ -2,12 +2,14 @@ package com.luv4code.beerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.luv4code.beerservice.web.model.BeerDto;
+import com.luv4code.beerservice.web.model.BeerStrleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -31,7 +33,7 @@ class BeerControllerTest {
 
     @Test
     void saveNewBeer() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getValidateBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(post("/api/v1/beer/")
@@ -42,12 +44,21 @@ class BeerControllerTest {
 
     @Test
     void updateBeerById() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getValidateBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(beerDtoJson))
                 .andExpect(status().isNoContent());
+    }
+
+    BeerDto getValidateBeerDto() {
+        return BeerDto.builder()
+                .beerName("My Beer")
+                .beerStyle(BeerStrleEnum.ALE)
+                .price(new BigDecimal("2.99"))
+                .upc(12344566431234L)
+                .build();
     }
 }
