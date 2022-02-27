09e697625be9ba27efa0e0840443b35fee3c2c99
luv4code-lc <madhav.p.akv@gmail.com>
2022-02-27 14:57:24 +0530

    adding ehcahe for list beer as well as get beer id

---
+++ src/main/java/com/luv4code/beerservice/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package com.luv4code.beerservice.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+}
--- src/main/java/com/luv4code/beerservice/services/BeerServiceImpl.java
+++ src/main/java/com/luv4code/beerservice/services/BeerServiceImpl.java
@@ -8,6 +8,7 @@ import com.luv4code.beerservice.web.model.BeerDto;
 import com.luv4code.beerservice.web.model.BeerPagedList;
 import com.luv4code.beerservice.web.model.BeerStyleEnum;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -23,8 +24,10 @@ public class BeerServiceImpl implements BeerService {
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
 
+    @Cacheable(cacheNames = "beerCache", key = "#beerId", condition = "#showInventoryOnHand == false ")
     @Override
     public BeerDto getBeerById(UUID beerId, Boolean showInventoryOnHand) {
+
 
         if (showInventoryOnHand) {
             return beerMapper.beerToBeerDtoWithInventory(beerRepository.findById(beerId).orElseThrow(NotFoundException::new));
@@ -50,8 +53,11 @@ public class BeerServiceImpl implements BeerService {
         return beerMapper.beerToBeerDto(beerRepository.save(beer));
     }
 
+    @Cacheable(cacheNames = "beerListCache", condition = "#showInventoryOnHand == false ")
     @Override
     public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand) {
+
+
         BeerPagedList beerPagedList;
         Page<Beer> beerPage;
 
