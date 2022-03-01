89cf200061cfa19bf422001c3b980febdcb148f1
luv4code-lc <madhav.p.akv@gmail.com>
2022-03-01 18:13:47 +0530

    implementing brewing service to trigger brewing;

---
+++ src/main/java/com/luv4code/beerservice/services/BrewingService.java
@@ -0,0 +1,45 @@
+package com.luv4code.beerservice.services;
+
+import com.luv4code.beerservice.config.JmsConfig;
+import com.luv4code.beerservice.domain.Beer;
+import com.luv4code.beerservice.events.BrewBeerEvent;
+import com.luv4code.beerservice.repositories.BeerRepository;
+import com.luv4code.beerservice.services.inventory.BeerInventoryService;
+import com.luv4code.beerservice.web.mapper.BeerMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BrewingService {
+
+    private final BeerRepository beerRepository;
+    private final BeerInventoryService beerInventoryService;
+    private final JmsTemplate jmsTemplate;
+    private final BeerMapper beerMapper;
+
+    @Scheduled(fixedRate = 5000)
+    public void checkForLowInventory() {
+        List<Beer> beers = beerRepository.findAll();
+
+        beers.forEach(beer -> {
+            Integer invQOH = beerInventoryService.getOnHandInventory(beer.getId());
+
+            log.debug("Min OnHand is: " + beer.getMinOnHand());
+            log.debug("Inventory is: " + invQOH);
+
+            if (beer.getMinOnHand() >= invQOH) {
+                jmsTemplate.convertAndSend(JmsConfig.BREWING_REQUEST_QUEUE, new BrewBeerEvent(beerMapper.beerToBeerDto(beer)));
+            }
+
+        });
+
+    }
+
+}
